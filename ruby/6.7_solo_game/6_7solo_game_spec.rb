91e620a57a51dbaa8197a8420b4ca7f5dd17d1ce
Adam Booth <4rlm@protonmail.ch>
2017-03-26 18:35:41 -0500

    created 6_7solo_game_spec.rb

---
+++ ruby/6.7_solo_game/6_7solo_game_spec.rb
@@ -0,0 +1,11 @@
+require_relative '6_7solo_game.rb'
+describe SoloGame do
+  it "gets player1 input" do
+  end
+
+  it "gets player2 input" do
+  end
+
+  it "proceses player1-2 data" do
+  end
+end
